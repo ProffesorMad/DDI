099f8172df80f396874936f2459e7b648383c8ac
Pablico <99720054+PoffesorMad@users.noreply.github.com>
2023-01-20 12:36:26 +0100

    Add files via upload

--- Spring-boot/Spring/src/main/java/com/in28minutes/springboot/web/model/Todo.java
+++ Spring-boot/Spring/src/main/java/com/in28minutes/springboot/web/model/Todo.java
@@ -4,9 +4,39 @@ import java.util.Date;
 
 public class Todo {
     private int id;
-    private String user;
-    private String desc;
-    private Date targetDate;
-    private boolean isDone;
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	private String user;
+    public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	private String desc;
+    public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	private Date targetDate;
+    public Date getTargetDate() {
+		return targetDate;
+	}
+	public void setTargetDate(Date targetDate) {
+		this.targetDate = targetDate;
+	}
+	private boolean isDone;
+	public boolean isDone() {
+		return isDone;
+	}
+	public void setDone(boolean isDone) {
+		this.isDone = isDone;
+	}
 
 } 
